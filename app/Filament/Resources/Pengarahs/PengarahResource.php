ddf84bd4fab350e6370f2d3b0343f6fb84bdf334
Endra Janitra <janitraendra@gmail.com>
2026-02-26 19:41:26 +0700

    fix: label pengarah

--- app/Filament/Resources/Pengarahs/PengarahResource.php
+++ app/Filament/Resources/Pengarahs/PengarahResource.php
@@ -15,12 +15,16 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use UnitEnum;
 
 class PengarahResource extends Resource
 {
     protected static ?string $model = Pengarah::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string | UnitEnum | null $navigationGroup = 'Surat Masuk';
+    protected static ?string $navigationLabel = 'Pengarah';
+
+    #protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
     protected static ?string $recordTitleAttribute = 'pengarah';
 
