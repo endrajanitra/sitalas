0eca6d1cabcbcbfcc8b6ae394331c8fdd7f84f90
Draa7 <janitraendra@gmail.com>
2026-03-12 14:46:09 +0700

    fix: input edit sopd pengajuan

--- app/Filament/Resources/SopdPengajuans/Schemas/SopdPengajuanForm.php
+++ app/Filament/Resources/SopdPengajuans/Schemas/SopdPengajuanForm.php
@@ -3,6 +3,11 @@
 namespace App\Filament\Resources\SopdPengajuans\Schemas;
 
 use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\FileUpload;
 
 class SopdPengajuanForm
 {
@@ -10,7 +15,26 @@ class SopdPengajuanForm
     {
         return $schema
             ->components([
-                //
+                DatePicker::make('tanggal_surat')
+                    ->required(),
+                Textarea::make('perihal')
+                    ->required()
+                    ->columnSpanFull(),
+                Select::make('sifat_surat_id')
+                    ->label('Sifat Surat')
+                    ->relationship('Sifat', 'sifat_surat')
+                    ->searchable()
+                    ->preload()
+                    ->required(),
+                TextInput::make('kepada')
+                    ->required(),
+                TextInput::make('kontak_person')
+                    ->required(),
+                Textarea::make('keterangan')
+                    ->required()
+                    ->columnSpanFull(),
+                FileUpload::make('upload_file')
+                    ->required(),
             ]);
     }
 }
