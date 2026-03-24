3827e5ac75c13aee112ee29c56ac21e5d628c179
Draa7 <janitraendra@gmail.com>
2026-03-24 23:35:45 +0700

    fix: tampilan form proposal

--- app/Filament/Resources/Proposals/Schemas/ProposalForm.php
+++ app/Filament/Resources/Proposals/Schemas/ProposalForm.php
@@ -6,6 +6,8 @@ use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\Select;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Schema;
 
 class ProposalForm
@@ -14,23 +16,67 @@ class ProposalForm
     {
         return $schema
             ->components([
-                DatePicker::make('tanggal')
-                    ->required(),
-                TextInput::make('no_reg')
-                    ->required(),
-                Select::make('direktorat_id')
-                    ->label('Unit Pengolah')
-                    ->relationship('unitPengolah', 'direktorat')
-                    ->searchable()
-                    ->preload()
-                    ->required(),
-                TextInput::make('pengirim')
-                    ->required(),
-                TextInput::make('perihal')
-                    ->nullable(),
-                Textarea::make('alamat')
-                    ->nullable()
-                    ->columnSpanFull(),
+
+                Grid::make([
+                    'default' => 1,
+                    'xl' => 2,
+                ])
+                ->columnSpanFull()
+                ->schema([
+
+                    // 🔵 KOLOM KIRI
+                    Grid::make(1)->schema([
+
+                        Section::make('Informasi Proposal')
+                            ->description('Data utama proposal')
+                            ->schema([
+                                Grid::make(2)->schema([
+
+                                    DatePicker::make('tanggal')
+                                        ->label('Tanggal')
+                                        ->required(),
+
+                                    TextInput::make('no_reg')
+                                        ->label('Nomor Registrasi')
+                                        ->required(),
+
+                                    Select::make('direktorat_id')
+                                        ->label('Unit Pengolah')
+                                        ->relationship('unitPengolah', 'direktorat')
+                                        ->searchable()
+                                        ->preload()
+                                        ->required()
+                                        ->columnSpanFull(),
+                                ]),
+                            ])
+                            ->extraAttributes(['class' => 'h-full']),
+                    ]),
+
+                    // 🟣 KOLOM KANAN
+                    Grid::make(1)->schema([
+
+                        Section::make('Detail Proposal')
+                            ->description('Informasi tambahan proposal')
+                            ->schema([
+                                Grid::make(2)->schema([
+
+                                    TextInput::make('pengirim')
+                                        ->label('Pengirim')
+                                        ->required(),
+
+                                    TextInput::make('perihal')
+                                        ->label('Perihal')
+                                        ->placeholder('Opsional'),
+
+                                    Textarea::make('alamat')
+                                        ->label('Alamat')
+                                        ->rows(3)
+                                        ->columnSpanFull(),
+                                ]),
+                            ])
+                            ->extraAttributes(['class' => 'h-full']),
+                    ]),
+                ]),
             ]);
     }
 }
