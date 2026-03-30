759e300d936f3fdf8682f900c7dc020fb4585e25
Salman Al Majali <undermod007@gmail.com>
2026-03-30 11:35:34 +0700

    fix: add direktorate_id to form data in CreateSopdPengajuan

--- app/Filament/Resources/SopdPengajuans/Pages/CreateSopdPengajuan.php
+++ app/Filament/Resources/SopdPengajuans/Pages/CreateSopdPengajuan.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\SopdPengajuans\Pages;
 
 use App\Filament\Resources\SopdPengajuans\SopdPengajuanResource;
+use Filament\Facades\Filament;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,7 +14,8 @@ class CreateSopdPengajuan extends CreateRecord
     protected function mutateFormDataBeforeCreate(array $data): array
     {
         $data['user_id'] = Auth::id();   // menyimpan user yang login
-        $data['is_sopd_req'] = true;     // menandai data berasal dari SOPD
+        $data['is_sopd_req'] = true;  
+        $data['direktorat_id'] = Filament::auth()->user()->direktorat_id;  // menandai data berasal dari SOPD
 
         return $data;
     }
